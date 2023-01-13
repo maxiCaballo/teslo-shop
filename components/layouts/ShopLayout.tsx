a804f297cdeb91bd3573ce1c1e777be75b109aab
maxiCaballo <caballomaxi@gmail.com>
2023-01-13 18:51:18 -0300

    Configuración estructura del proyecto - instalacion de dependencias - Layout principial - componente navbar

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,45 @@
+import { FC, ReactElement } from 'react';
+import Head from 'next/head';
+import { Navbar } from '../ui/';
+
+type Props = {
+  children: ReactElement;
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string; //Cuando quiera compartir en redes sociales
+};
+
+export const ShopLayout: FC<Props> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={pageDescription} />
+
+        {/*Etiquetas para proporcionar info a las redes sociales  */}
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={title} />
+        {imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+      </Head>
+
+      <Navbar />
+
+      {/*TODO: Sidebar */}
+      <main
+        style={{
+          margin: '80px auto',
+          maxWidth: '1440px',
+          padding: '0px 30px',
+        }}
+      >
+        {children}
+      </main>
+      <footer>{/* TODO: Custom footer */}</footer>
+    </>
+  );
+};
---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,67 @@
+import NextLink from 'next/link';
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+  Badge,
+} from '@mui/material';
+import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
+
+export const Navbar = () => {
+  return (
+    <AppBar>
+      <nav>
+        <Toolbar>
+          <NextLink href="/" passHref legacyBehavior>
+            <Link display="flex" alignItems="center">
+              <Typography variant="h6">Teslo |</Typography>
+              <Typography sx={{ ml: 0.5 }}>Shop</Typography>
+            </Link>
+          </NextLink>
+
+          <Box sx={{ flex: 1 }} />
+
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <NextLink href="/category/men" passHref legacyBehavior>
+              <Link>
+                <Button>Mens</Button>
+              </Link>
+            </NextLink>
+            <NextLink href="/category/women" passHref legacyBehavior>
+              <Link>
+                <Button>Womens</Button>
+              </Link>
+            </NextLink>
+            <NextLink href="/category/kid" passHref legacyBehavior>
+              <Link>
+                <Button>kids</Button>
+              </Link>
+            </NextLink>
+          </Box>
+
+          <Box sx={{ flex: 1 }} />
+
+          <IconButton>
+            <SearchOutlined />
+
+            <NextLink href="/cart" passHref legacyBehavior>
+              <Link>
+                <IconButton>
+                  <Badge badgeContent={2} color="secondary">
+                    <ShoppingCartOutlined />
+                  </Badge>
+                </IconButton>
+              </Link>
+            </NextLink>
+          </IconButton>
+
+          <Button>Menú</Button>
+        </Toolbar>
+      </nav>
+    </AppBar>
+  );
+};
